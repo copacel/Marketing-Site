ba31bdfbc74da2a66c607e95ff626622d82c4df9
Marius Copacel <marius.copacel@tremend.com>
2022-08-17 11:56:09 +0300

    implemet marketing site on product list, product details and wishlist

--- cartridges/race_marketing_site/cartridge/scripts/helpers/sitePreferences.js
+++ cartridges/race_marketing_site/cartridge/scripts/helpers/sitePreferences.js
@@ -1,10 +1,32 @@
 var System = require('dw/system');
+var marketingSiteEnabledCachedValue = 0;
 
+/**
+ *
+ * @returns {boolean} a boolean of true or false
+ */
 function isMarketingSiteEnabled() {
+    if (marketingSiteEnabledCachedValue !== 0) {
+        return marketingSiteEnabledCachedValue;
+    }
     var site = System.Site.getCurrent();
-    return site.getCustomPreferenceValue('marketingSiteEnabled') ? true : false;
+    var isMarketingSiteEnabled = site.getCustomPreferenceValue('enableMarketingSite') ? true : false;
+    marketingSiteEnabledCachedValue = isMarketingSiteEnabled;
+
+    return isMarketingSiteEnabled;
 }
 
+/**
+ * Appends site preference values to controller view data
+ * @param {Object} res - the response from the controller to append values to
+ */
+function appendSitePreferences(res) {
+    var viewData = res.getViewData();
+    viewData.isMarketingSiteEnabled = isMarketingSiteEnabled();
+    res.setViewData(viewData);
+};
+
 module.exports = {
-    isMarketingSiteEnabled: isMarketingSiteEnabled
+    isMarketingSiteEnabled: isMarketingSiteEnabled,
+    appendSitePreferences: appendSitePreferences
 }
